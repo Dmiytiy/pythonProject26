cd11f7fd17ff236bc4f9198fa52e3c7a1b991e34
Dmiytiy <prostoy.general@gmail.com>
2023-04-09 14:59:25 +0300

    Initial commit

---
+++ main/__init__.py
@@ -0,0 +1,7 @@
+# from flask import Blueprint, request, render_template
+#
+# main.blueprint = Blueprint('main', __name__, template_folder='templates ')
+#
+# @main.blueprint.route('/')
+# def main():
+#     return render_template('index.py')
